9e6b40d2ac1219093d77b2a0d1e87b3c8d0c4a80
JonahM-coder <71911673+JonahM-coder@users.noreply.github.com>
2023-04-25 15:26:36 -0700

    Added level exit script

---
+++ Assets/Scripts/LevelExitController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class LevelExitController : MonoBehaviour
+{
+    public int nextScene = 0;
+
+    //Affected Level Object Variables
+    public bool isTriggered;
+
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = false;
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D()
+    {
+        if (!isTriggered)
+        {
+            isTriggered = true;
+            SceneManager.LoadScene(nextScene);
+
+        }
+
+    }
+}
