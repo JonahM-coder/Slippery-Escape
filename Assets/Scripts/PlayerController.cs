98b63fd2185acc9858b825573fa8de6a227b9ac2
jrmorris29 <89809723+jrmorris29@users.noreply.github.com>
2023-04-25 16:55:44 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,46 @@ public class PlayerController : MonoBehaviour {
     public HideablePlayer hideablePlayer;
     public float moveSpeed = 2f;
     public float jumpForce = 3f;
+    
+    private Collider2D[] results;
+    private Vector2 direction;
 
+    private bool climbing;
     private Rigidbody2D _rb;
 
     void Start() {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    void CheckCollision() {
+        climbing = false;
+
+         Vector2 size = GetComponent<Collider>().bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = results[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Pipe"))
+            {
+                climbing = true;
+            }
+        }
+
+    }
+
     void Update() {
+       CheckCollision();
+
+        if (climbing)
+    {
+        direction.y = Input.GetAxis("Vertical") * moveSpeed;
+    }
+       
         if (Input.GetKeyDown(KeyCode.Q)) {
             if (hideablePlayer.IsHidden) {
                 hideablePlayer.Unhide();
