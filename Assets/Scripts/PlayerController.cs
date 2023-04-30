78932fee3d1ad4eb7ec5d48e24375aa51fa7159c
Vivian-Trieu <vivian.dtrieu@gmail.com>
2023-04-30 14:59:23 -0700

    Added hiding tubes and escape vents
    Also includes animation frames for Hank hiding in tube but thats optional

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,29 +15,10 @@ public class PlayerController : MonoBehaviour {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    void CheckCollision() {
-        climbing = false;
-
-         Vector2 size = GetComponent<Collider>().bounds.size;
-        size.y += 0.1f;
-        size.x /= 2f;
-
-        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
-
-        for (int i = 0; i < amount; i++)
-        {
-            GameObject hit = results[i].gameObject;
-
-            if (hit.layer == LayerMask.NameToLayer("Pipe"))
-            {
-                climbing = true;
-            }
-        }
-
-    }
+    
 
     void Update() {
-       CheckCollision();
+       // CheckCollision();
 
         if (climbing)
     {
