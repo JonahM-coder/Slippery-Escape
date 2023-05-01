ba0e7541ba344a9119e28e038c25d25babef7f34
dtchitt <danielchittenden@yahoo.com>
2023-05-01 13:45:55 -0700

    move scripts to scripts folder

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    
+    //Affected Level Object Variables
+    public GameObject target;
+    public bool isTriggered;
+    public float newPos = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = false;
+    }
+
+    // Update is called once per frame
+    public void OpenLever()
+    {
+        if(!isTriggered)
+        {
+            isTriggered = true;
+            target.transform.Translate(0, newPos, 0f);
+            
+        }
+    }
+}
