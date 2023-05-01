84cb0cc8137854237f535a79316d755c46498bbf
chungsamuel <samuelqchung@gmail.com>
2023-05-01 15:54:43 -0700

    new bgms and footstep sounds
    edited footstep script  to include hiding, added some bgms to scene 2

--- Assets/SFX/footsteps.cs
+++ Assets/SFX/footsteps.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class footsteps : MonoBehaviour
 {
     public AudioSource footstepSounds;
+    public HideablePlayer hideablePlayer;
 
     // Update is called once per frame
     void Update()
     {
+        if (hideablePlayer.IsHidden) {
+            footstepSounds.enabled = false;
+            return;
+        }
+
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
             footstepSounds.enabled = true;
         } else {
             footstepSounds.enabled = false;
         }
+        
     }
 }
